7fb0dd26b51782c292fb33984a7750968cc7b164
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 13:19:45 +0900

    annot xml add

--- src/main/java/myspring/di/annot/HelloBean.java
+++ src/main/java/myspring/di/annot/HelloBean.java
@@ -2,9 +2,20 @@ package myspring.di.annot;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloBean {
+	@Value("어노테이션")
 	String name;
+	
+	@Autowired
+	@Qualifier("stringPrinter")
 	IPrinter printer;
+	
 	List<String> names;
 
 	public HelloBean() {
@@ -24,16 +35,6 @@ public class HelloBean {
 
 	public void setNames(List<String> list) {
 		this.names = list;
-	}
-
-	public void setName(String name) {
-		System.out.println("setName() 메서드 호출됨 " + name);
-		this.name = name;
-	}
-
-	public void setPrinter(IPrinter printer) {
-		System.out.println("setPrinter() 메서드 호출됨 " + printer.getClass().getName());
-		this.printer = printer;
 	}
 
 	public String sayHello() {
