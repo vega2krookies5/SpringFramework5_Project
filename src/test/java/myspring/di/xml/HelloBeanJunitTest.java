66a389eec7fee7570ed6cafc9520a56b6806c2a2
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 09:39:40 +0900

    HelloBeanJunitTest update

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,11 +1,14 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
 
 	@Test
 	void helloBean() {
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
 		
 	}
 }
