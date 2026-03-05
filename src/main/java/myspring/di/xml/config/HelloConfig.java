90540855e58f6093b94e0361292ad645b9600225
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 16:47:21 +0900

    HelloConfig add

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,8 +1,10 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import myspring.di.xml.ConsolePrinter;
 import myspring.di.xml.Hello;
@@ -12,6 +14,9 @@ import myspring.di.xml.StringPrinter;
 @Configuration
 @PropertySource(value = "classpath:values.properties")
 public class HelloConfig {
+	
+	@Autowired
+	Environment environment;
 	
 	@Bean
 	public Printer stringPrinter() {
@@ -25,7 +30,11 @@ public class HelloConfig {
 	
 	@Bean
 	public Hello hello() {
-		
+		Hello hello = new Hello();
+		//"자바컨피그"
+		hello.setName(environment.getProperty("myName3"));
+		hello.setPrinter(stringPrinter());
+		return hello;
 	}
 	
 }
