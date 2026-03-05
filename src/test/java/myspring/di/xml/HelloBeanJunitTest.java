cf1a8eb7fb704ff66b758de84160a54bb932dc8e
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 09:50:51 +0900

    HelloBeanJunitTest update

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,14 +1,21 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
+	ApplicationContext context;
+	
+	@BeforeEach
+	void init() {
+		//Container 객체생성
+		context = new GenericXmlApplicationContext("classpath:hello-bean.xml");		
+	}
 
 	@Test
 	void helloBean() {
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
 		
 	}
 }
