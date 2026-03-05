cfd8378c287472c179874eac77ad67a31aabbea7
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 15:32:42 +0900

    HelloBeanConfig add

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -1,8 +1,11 @@
 package myspring.di.annot.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//<context:component-scan base-package="myspring.di.annot" />
+@ComponentScan(basePackages = {"myspring.di.annot"})
 public class HelloBeanConfig {
 
 }
