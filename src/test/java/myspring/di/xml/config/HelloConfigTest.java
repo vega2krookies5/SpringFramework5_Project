5c59f6c00fe76549a6462551f8f266a33e6b2cf7
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 16:54:58 +0900

    HelloConfig add

--- src/test/java/myspring/di/xml/config/HelloConfigTest.java
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -8,7 +8,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.annotation.Resource;
+
 import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = HelloConfig.class)
@@ -16,8 +19,13 @@ public class HelloConfigTest {
 	@Autowired
 	Hello hello;
 	
+	@Resource(name="stringPrinter")
+	Printer printer;
+	
 	@Test
 	void hello() {
 		assertEquals("Hello 자바컨피그", hello.sayHello());
+		hello.print();
+		
 	}
 }
