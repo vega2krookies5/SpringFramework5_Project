37cfce910c8aa36655996b951e3242d7e201bdaf
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 13:44:19 +0900

    SpringTest add

--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -1,11 +1,22 @@
 package myspring.di.annot;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:hello-annot.xml")
 public class HelloBeanSpringTest {
+	@Autowired
+	HelloBean hello;
 	
+	@Test
+	void helloBeanSetter() {
+		assertEquals("Hello 어노테이션", hello.sayHello());
+		
+	}
 }
