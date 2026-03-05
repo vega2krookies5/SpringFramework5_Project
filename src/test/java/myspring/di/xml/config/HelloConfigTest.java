975ab59859a259bb23da7e3d14ea29b6ee81e562
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-05 16:52:42 +0900

    HelloConfig add

--- src/test/java/myspring/di/xml/config/HelloConfigTest.java
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import myspring.di.xml.Hello;
 
 @ExtendWith(SpringExtension.class)
